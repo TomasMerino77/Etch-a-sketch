9e024f235fe523899dd4da2fa3929a88b8c37b6c
Tomas Merino <tomasmerinomm@gmail.com>
2025-06-14 19:18:42 +0000

    Implements working resizing of the board

--- javascript.js
+++ javascript.js
@@ -1,22 +1,44 @@
 const container = document.querySelector("#container");
-// const square = document.createElement("div");
+let square;
 
-function Grid() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+function Grid(size = 16) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
             container.appendChild(square);
-            // square.textContent = `${i}, ${j}`;
         }
     }
+    square = document.querySelectorAll(".square");
 }
-
 Grid();
 
-const square = document.querySelectorAll(".square");
-square.forEach((square) => 
-    square.addEventListener("mouseover", () => {
-        console.log("hovered");
-        square.style.backgroundColor = "blue";
-}));
+
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "blue";
+    }
+});
+
+const sizeButton = document.querySelector("#selector");
+sizeButton.addEventListener("click", () => {
+    //Choose the size
+    let boardSize = prompt("Select the size of the board. Enter a number between 2 and 100");
+    if (boardSize === null) return;
+    while (isNaN(boardSize) || boardSize < 2 || boardSize > 100) { //TODO ESC to cancel and leave as it is rn
+        boardSize = prompt("Incorrect imput, select a value between 2 and 100")
+        if (boardSize === null) return;
+    }
+    //Eliminates the old board
+    square.forEach((sqr) => {
+        container.removeChild(sqr);
+    });
+    //Calls the function Grid with the new size
+    Grid(boardSize);
+    //Changes the size of the squares
+    let sqrSize = 900 / boardSize;
+    square.forEach((sqr) => {
+        sqr.style.width = `${sqrSize}px`;
+        sqr.style.height = `${sqrSize}px`;
+    })
+});
